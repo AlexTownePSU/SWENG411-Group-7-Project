6bf3533f1d09664c2e56e74e8980711f281ba740
Stowne518 <samuel.towne@centralsquare.com>
2025-06-02 00:14:10 -0400

    Uploading starter files
    Uploading the beginning files and nodes that I created for the Project repository.

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const hostname = '192.168.86.158';
+const port = 3000;
+const app = express();
+
+// Serve static files (like index.html)
+app.use(express.static(path.join(__dirname)));
+
+// Set CORS options
+const corsOptions = {
+	origin: `http://${hostname}:${port}`,
+	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+	credentials: true,
+};
+app.use(cors(corsOptions));
+
+// MongoDB connection
+const uri = "mongodb+srv://group7db:lSSiu4rXTW0Sh2u4@cluster0.ve13uvk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	}
+});
+
+let db;		// shared DB reference
+
+// Connect when server starts
+async function connectToDB() {
+	try {
+		await client.connect();
+		db = client.db('sample_mflix');
+		console.log('Connected to MongoDB');
+	} catch (err) {
+		console.error('Failed to connected to MongoDB:', err);
+	}
+}
+// API endpoints
+app.use(express.json());	// Allows for parsing JSON bodies
+// GET Handler -- ONLY for movie titles as a test
+app.get('/api/data', async (req, res) => {
+	try {
+		const collection = db.collection('movies');
+		const data = await collection.find().limit(50).toArray(); // Limit for testing
+		res.json(data);
+	} catch (error) {
+		console.error(error);
+		res.status(500).send('Error fetching data');
+	} finally {
+		await client.close();
+	}
+});
+
+// Connect to DB then Start server
+connectToDB().then(() => {
+	app.listen(port, hostname, () => {
+		console.log(`Server running at http://${hostname}:${port}/`);
+	});
+});
